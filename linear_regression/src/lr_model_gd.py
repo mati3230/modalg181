9cce32624cef2e952115899c596e6f16c013001d
Marcel Tiator <marcel.tiator@study.hs-duesseldorf.de>
2018-05-16 11:18:04 +0200

    added linear regression solution

--- linear_regression/src/lr_model_gd.py
+++ linear_regression/src/lr_model_gd.py
@@ -5,19 +5,40 @@ Created on 04.05.2018
 '''
 import tensorflow as tf
 
+def tile_theta(theta, batch_size=2):
+    bs = tf.constant([batch_size])
+    return tf.reshape(tf.tile(theta, bs), [ bs[0], tf.shape(theta)[0]])
+
 def inference(x, batch_size=1, theta=None):
     # TODO compute prediction
-    return tf.constant(0)
+    if batch_size > 1:
+        theta = tile_theta(theta=theta, batch_size=batch_size)
+    return tf.reduce_sum( tf.multiply( x, theta ), 1, keepdims=True )
 
 def loss(y_pred, y_true, batch_size=1):
     # TODO compute error
-    return tf.constant(1)
+    y_pred = tf.reshape(y_pred, [batch_size, 1])
+    y_true = tf.reshape(y_true, [batch_size, 1])
+    return tf.losses.mean_squared_error(y_true, y_pred)
 
 def gradient_descent(x, y_true, theta, batch_size=1, n_features=4, learning_rate=0.001):
     # TODO implement gradient descent
     # TODO ignore batch_size
     y_pred = inference(x=x, batch_size=batch_size, theta=theta)
+    # print(y_pred.get_shape())
     # TODO change theta
+    y_true = tf.scalar_mul(-1.0, y_true)
+    y_true = tf.reshape(y_true, (batch_size, 1))
+    residual = tf.add(y_pred, y_true)  
+    residual = tf.scalar_mul(learning_rate/batch_size, residual)
+    if batch_size > 1:
+        x_transpose = tf.transpose(x)
+        residual = tf.matmul(x_transpose, residual)
+    else:
+        residual = tf.multiply(x, residual)
+    residual = tf.reshape(residual, [n_features])
+    theta = tf.subtract(theta, residual)
+    theta = tf.reshape(theta, [n_features])
     return y_pred, theta
     
 def train(x, y_true, theta, batch_size=1, n_features=4, learning_rate=0.001):
